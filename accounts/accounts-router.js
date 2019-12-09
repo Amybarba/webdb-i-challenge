1bc92fbbba4043b5a90e5a14e02ac49296e40997
Amy Barba <Amybarba70@gmail.com>
2019-12-08 22:59:28 -0800

    crud file created

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -2,17 +2,80 @@ const router = require('express').Router();
 
 const db = require('../data/dbConfig.js');
 
-router.get
+router.get('/', (req, res) => {
+  db('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'List of Accounts not found' });
+    });
+});
 
-router.get
+router.get('/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+        if (account) {
+            res.status(200).json(account);
+        } else {
+            res.status(404).json({ message: 'Account not found' });
+        }
+    });
+});
 
-router.post
+router.post('/', (req, res) => {
+    if (accountIsValid(req.body)) {
+        db('accounts')
+        .insert(req.body, 'id')
+        .then(([id]) => id)
+        .then(id => {
+            db('accounts')
+            .where({ id })
+            .first()
+            .then(account => {
+                res.status(201).json(account);
+            });
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Account was not added ' });
+        });
+    } else {
+        res.status(400).json({
+            message: 'Please provide name and budget of zero or more to locate account',
+        });
+    }
+});
 
+router.put('/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+        if (count) {
+            res.status(404).json({ message: 'Account not located' });
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Account could not be updated' });
+    });
+});
 
-router.put
+router.delete('/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+        res.status(200).json({ message: `${count} record(s) deleted` });
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Account was not removed' });
+    });
+});
 
-
-router.delete
-
+function accountIsValid({ name, budget }) {
+    return name && typeof budget === 'number' && budget >= 0l
+}
 
 module.exports = router;
