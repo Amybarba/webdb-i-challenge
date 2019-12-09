2e94b9977def7cc5c4bd71a0bc5e7f15598e6f2d
Amy Barba <Amybarba70@gmail.com>
2019-12-08 22:32:29 -0800

    marked read me for completion, corrected uppercase in queries, coded the server file, created the structure for the router file

---
+++ accounts/accounts-router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const db = require('../data/dbConfig.js');
+
+router.get
+
+router.get
+
+router.post
+
+
+router.put
+
+
+router.delete
+
+
+module.exports = router;
--- queries.sql
+++ queries.sql
@@ -6,7 +6,7 @@ SELECT * FROM Customers WHERE PostalCode=1010;
 
 -- Find the *phone* number for the *supplier* with the *id 11*
 
-SELECT Phone FROM Suppliers where SupplierID=11;
+SELECT Phone FROM Suppliers WHERE SupplierID=11;
 
 -- List *first 10 **orders** placed, sorted **descending** by the **order date
 
@@ -14,7 +14,7 @@ SELECT * FROM Orders ORDER BY OrderDate DESC LIMIT 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
-SELECT * FROM Customers where City='London' OR City='Madrid' OR City='Brazil'
+SELECT  * FROM Customers WHERE City='London' OR City='Madrid' OR City='Brazil'
 
 -- Add a customer record for "The Shire", the contact 
 -- name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", 
