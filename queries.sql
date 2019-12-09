9b45b6f6fdcf6685336bfb88f25ca9f0d92fc89f
Amy Barba <Amybarba70@gmail.com>
2019-12-08 20:18:57 -0800

    reviewed training kit, recoded queries including stretch for better understanding

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 -- Find all customers with postal code 1010
 
-SELECT * FROM Customers where PostalCode=1010;
+SELECT * FROM Customers WHERE PostalCode=1010;
 
 -- Find the *phone* number for the *supplier* with the *id 11*
 
@@ -21,7 +21,7 @@ SELECT * FROM Customers where City='London' OR City='Madrid' OR City='Brazil'
 -- postal code "111" and the country is "Middle Earth"
 
 INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country)
-VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth') 
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
@@ -33,8 +33,16 @@ WHERE ContactName = 'Bilbo Baggins';
 -- (Stretch) Find a query to discover how many different cities are 
 -- stored in the Customers table. Repeats should not be double counted
 
-
-
+SELECT 
+    DISTINCT City
+FROM
+    Customers
+ORDER BY 
+    City;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. 
 -- You can use `length(SupplierName)` to get the length of the name
+
+SELECT SupplierName 
+FROM Suppliers
+WHERE LENGTH(SupplierName)> 20;
